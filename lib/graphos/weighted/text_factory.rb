91cc6f61968e7c9aae7203d8f97e5359428800db
Bernardo Amorim <contato@bamorim.com>
2014-11-12 12:39:49 -0200

    IO.readlines

--- lib/graphos/weighted/text_factory.rb
+++ lib/graphos/weighted/text_factory.rb
@@ -2,9 +2,9 @@ module Graphos
   module Weighted
     module TextFactory
       def self.read path
-        f = File.open(path,"r")
-        graph = Graph.new(f.gets.to_i)
-        while(line=f.gets)
+        lines = IO.readlines(path)
+        graph = Graph.new(lines[0].to_i)
+        lines[1..-1].each do |line|
           args = line.split(/[ \n]+/)
           graph.add_edge args[0].to_i-1, args[1].to_i-1, args[2].to_f
         end
